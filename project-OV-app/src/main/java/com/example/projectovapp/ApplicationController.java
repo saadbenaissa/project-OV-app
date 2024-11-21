ed95fa981f997867de01dd0a596f5231ebffa26a
DionvdEerden <dion.vandereerden@student.hu.nl>
2024-11-21 13:39:01 +0100

    branch Dion - views

--- project-OV-app/src/main/java/com/example/projectovapp/ApplicationController.java
+++ project-OV-app/src/main/java/com/example/projectovapp/ApplicationController.java
@@ -1,15 +1,33 @@
 package com.example.projectovapp;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+
+
+import java.io.IOException;
 
 public class ApplicationController {
 
-    @FXML
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welkom bij OVerzichtelijk!");
+    public void onRoutePlannerButtonClick(ActionEvent event) throws IOException
+    {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("route.fxml"));
+        Stage stage = new Stage();
+        stage.setScene(new Scene(loader.load()));
+        stage.setTitle("route planner");
+        stage.show();
     }
 }

d99b46327c9dcaf4dc55bc978f23a03377ad808d
DionvdEerden <dion.vandereerden@student.hu.nl>
2024-11-21 12:17:45 +0100

    branch Dion

--- project-OV-app/src/main/java/com/example/projectovapp/ApplicationOpen.java
+++ project-OV-app/src/main/java/com/example/projectovapp/ApplicationOpen.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ApplicationOpen extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ApplicationOpen.class.getResource("open.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("OVerzichtelijk");
         stage.setScene(scene);
         stage.show();
     }

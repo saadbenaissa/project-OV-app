92e390dc93e1fec0d1db3eefd0b908355644e3af
saadbenaissa <saad.benaissa2000@gmail.com>
2024-12-18 20:00:47 +0100

    Login functionaliteit toegevoegd en het werkt eindelijk

--- project-OV-app/src/main/java/com/example/projectovapp/ApplicationController.java
+++ project-OV-app/src/main/java/com/example/projectovapp/ApplicationController.java
@@ -36,6 +36,13 @@ public class ApplicationController {
         ApplicationOpen.ChangeScene2((Stage) testText.getScene().getWindow());
     }
 
+    // Login knop
+    @FXML
+    public void onLoginButtonClick(ActionEvent event) throws IOException {
+        // Verander naar de registratiepagina
+        ApplicationOpen.ChangeToLoginPage((Stage)testText.getScene().getWindow());
+    }
+
     // Registatie knop
     @FXML
     public void onRegisterButtonClick(ActionEvent event) throws IOException {
@@ -43,14 +50,14 @@ public class ApplicationController {
         ApplicationOpen.ChangeToRegistrationPage((Stage)testText.getScene().getWindow());
     }
 
-    @FXML
-    private Label beginStationLabel;
+//    @FXML
+//    private Label beginStationLabel;
 
-    @FXML
-    private Label endStationLabel;
+//    @FXML
+//    private Label endStationLabel;
 
-    public void initialize() {
-        beginStationLabel.setText("Amsterdam Centraal");
-        endStationLabel.setText("Utrecht Centraal");
-    }
+//    public void initialize() {
+//        beginStationLabel.setText("Amsterdam Centraal");
+//        endStationLabel.setText("Utrecht Centraal");
+//    }
 }
